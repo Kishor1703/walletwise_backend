40ce1220c83418d07225611ca955038478650e8d
Kishor1703 <pskishor196@gmail.com>
2026-01-21 13:40:28 +0530

    Error Solved

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    // email: { type: String, required: true, unique: true },
+    email: { type: String, required: false, unique: true },
     createdAt: { type: Date, default: Date.now }
 });
 
