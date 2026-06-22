2404020767da86fc3a3e33095e4647f32b9708c3
Kishor1703 <pskishor196@gmail.com>
2026-06-22 18:16:56 +0530

    bug fixed

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const { encrypt, decrypt } = require('../utils/encryption');
 
 const transactionSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
@@ -15,48 +14,12 @@ const transactionSchema = new mongoose.Schema({
 });
 
 /* 🔐 Encrypt BEFORE saving to DB */
-transactionSchema.pre('save', function (next) {
-  if (!this.isModified()) {
-    return next();
-  }
-
-  if (this.isModified('amount')) {
-    this.amount = encrypt(this.amount.toString());
-  }
-
-  // if (this.isModified('category') && this.category) {
-  //   this.category = encrypt(this.category);
-  // }
-
-  if (this.isModified('type')) {
-    this.type = encrypt(this.type);
-  }
-
-  if (this.isModified('description') && this.description) {
-    this.description = encrypt(this.description);
-  }
-
-  if (this.isModified('person')) {
-    this.person = encrypt(this.person);
-  }
-
-  next();
-});
 
 /* 🔓 Decrypt AFTER fetching from DB */
 transactionSchema.methods.toJSON = function () {
   const obj = this.toObject();
 
-  try {
-    obj.amount = Number(decrypt(obj.amount));
-    // obj.category = obj.category ? decrypt(obj.category) : obj.category;
-    obj.type = decrypt(obj.type);
-    obj.description = obj.description ? decrypt(obj.description) : obj.description;
-    obj.person = decrypt(obj.person);
-  } catch (err) {
-    console.error('Error decrypting transaction fields:', err);
-    throw new Error('Failed to decrypt transaction data');
-  }
+  obj.amount = Number(obj.amount);
 
   return obj;
 };
